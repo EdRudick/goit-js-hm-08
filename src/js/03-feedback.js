5c00b0fe0c4c1f81e8d49342a45f517d8e35d11f
EdRudick <erudnytdky@gmail.com>
2023-09-23 14:21:54 +0300

    2309

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+let objOfValue = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {
+  email: '',
+  message: '',
+};
+
+const { email, message } = formRef.elements;
+
+email.value = objOfValue.email.trim();
+message.value = objOfValue.message.trim();
+
+formRef.addEventListener('input', throttle(onInputForm, 500));
+formRef.addEventListener('submit', onSubmitForm);
+
+function onInputForm(event) {
+  objOfValue = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(objOfValue));
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log('User information:\n', objOfValue);
+
+  event.currentTarget.reset();
+}
