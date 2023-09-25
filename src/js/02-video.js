1ae27bfc1af030db7a1980332a12a26579d9af74
EdRudick <erudnytdky@gmail.com>
2023-09-25 22:49:23 +0300

    4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new player(iframe);
+const player = new Player(iframe);
 
 player.on(
   'timeupdate',
